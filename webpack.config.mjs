a194e3e62cd5165156617ce34f37f1a929124a6a
bybygo <bbgo.lee@gmail.com>
2025-02-07 03:30:33 +0800

    chore: optimize webpack build process

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,9 +1,14 @@
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import Dotenv from 'dotenv-webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path from 'path';
+import TerserPlugin from 'terser-webpack-plugin';
 import { fileURLToPath } from 'url';
+import webpack from 'webpack';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -17,17 +22,49 @@ export default (env, argv) => {
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       path: path.resolve(__dirname, 'dist'),
       clean: true,
+      publicPath: '/',
     },
     devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
     module: {
       rules: [
         {
           test: /\.(sa|sc|c)ss$/i,
-          use: ['style-loader', 'css-loader', 'postcss-loader'],
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                sourceMap: true,
+                modules: {
+                  auto: true,
+                  localIdentName: isProduction ? '[hash:base64]' : '[path][name]__[local]',
+                },
+              },
+            },
+            'postcss-loader',
+          ],
         },
         {
-          test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-          type: 'asset',
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[hash][ext][query]',
+          },
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[hash][ext][query]',
+          },
+        },
+        {
+          test: /\.svg$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'icons/[hash][ext][query]',
+          },
         },
         {
           test: /\.(ts|js)x?$/,
@@ -52,7 +89,53 @@ export default (env, argv) => {
         template: './public/index.html',
       }),
       new Dotenv(),
-    ],
+      new MiniCssExtractPlugin({
+        filename: isProduction ? 'css/[name].[contenthash].css' : 'css/[name].css',
+        chunkFilename: isProduction ? 'css/[id].[contenthash].css' : 'css/[id].css',
+        ignoreOrder: false,
+      }),
+      new webpack.ProgressPlugin((percentage, message, ...args) => {
+        // eslint-disable-next-line no-console
+        console.log(`${(percentage * 100).toFixed(2)}%`, message, ...args);
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
+      }),
+    ].filter(Boolean),
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+      minimize: isProduction,
+      minimizer: [
+        new CssMinimizerPlugin(),
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: isProduction,
+            },
+          },
+        }),
+      ],
+    },
+    cache: {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    },
+    performance: {
+      hints: isProduction ? 'warning' : false,
+      maxAssetSize: 512000,
+      maxEntrypointSize: 512000,
+    },
     devServer: {
       static: {
         directory: path.join(__dirname, 'public'),
@@ -64,6 +147,14 @@ export default (env, argv) => {
       historyApiFallback: true,
       headers: {
         'Access-Control-Allow-Origin': '*',
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'DENY',
+      },
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
       },
     },
   };
